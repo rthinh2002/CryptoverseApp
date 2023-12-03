12ce96131cad3bb1b0d492dd675af568382c03a0
rthinh2002 <rthinh2002@gmail.com>
2023-12-03 12:05:46 +1030

    Fixed news api

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { Select, Typography, Row, Col, Card } from 'antd';
 import moment from 'moment';
 
